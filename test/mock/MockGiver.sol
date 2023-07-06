8883b5b13220918392f61b6b67088c645848c2dd
KONFeature <quentin@nivelais.com>
2023-07-06 15:14:24 +0200

    ⬆️ Upgrade to solidity 0.8.20

--- test/mock/MockGiver.sol
+++ test/mock/MockGiver.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.20;
 
 import {IGiver} from "src/interfaces/IGiver.sol";
 import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
